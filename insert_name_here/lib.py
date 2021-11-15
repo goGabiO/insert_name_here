6f92c94669b1ee08969d56b073ae1b5f232aa208
Gabriella Otero <gabriella.otero@gmail.com>
2021-11-15 13:01:56 +0100

    Initial Push

---
+++ insert_name_here/lib.py
@@ -0,0 +1,8 @@
+def try_me():
+    a = 'working'
+    return f"I think I am {a}"
+
+
+if __name__ == "__main__":
+    tried = try_me()
+    print(tried)
---
+++ tests/test_lib.py
@@ -0,0 +1,5 @@
+from insert_name_here.lib import try_me
+
+
+def test_try_me():
+    assert try_me() == "I think I am working"
